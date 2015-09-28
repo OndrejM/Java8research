52fb3ed2056e478ea1494b6288a98feedf0403a2
ondro <ondro@ondro-lenovo-flex>
2015-09-29 01:30:43 +0200

    Making lambdas serializable (with intention to use https://github.com/TrigerSoft/jaque)

---
+++ src/test/java/eu/ondrom/java8/lambda/Serializing.java
@@ -0,0 +1,54 @@
+package eu.ondrom.java8.lambda;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Serializing {
+    
+    public static interface SerializableRunnable extends Runnable, Serializable {
+    }
+    
+    @Test
+    public void serializableLambdaCanBeSerialized() throws IOException, ClassNotFoundException {
+        Runnable r = (Runnable & Serializable)() -> System.out.println("Serializable!");
+        givenLambdaCanBeSerialized(r);
+    }
+
+    @Test(expected = NotSerializableException.class)
+    public void anyLambdaCannotBeSerialized() throws IOException, ClassNotFoundException {
+        Runnable r = () -> System.out.println("Serializable!");
+        givenLambdaCanBeSerialized(r);
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void anyLambdaCannotBeEasilyMadeSerializable() throws IOException, ClassNotFoundException {
+        Runnable r = () -> System.out.println("Serializable!");
+        Runnable sr = (Runnable & Serializable)r;
+        givenLambdaCanBeSerialized(sr);
+    }
+
+    @Test
+    public void anyLambdaExprCanBeMadeSerializable() throws IOException, ClassNotFoundException {
+        SerializableRunnable sr = () -> System.out.println("Serializable!");
+        givenLambdaCanBeSerialized(sr);
+    }
+
+    private void givenLambdaCanBeSerialized(Runnable r) throws ClassNotFoundException, IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        new ObjectOutputStream(out).writeObject(r);
+        Object o = new ObjectInputStream(new ByteArrayInputStream(out.toByteArray())).readObject();
+        assertNotNull(o);
+        assertEquals(r.getClass().getPackage(), o.getClass().getPackage());
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new Serializing().serializableLambdaCanBeSerialized();
+    }
+}
